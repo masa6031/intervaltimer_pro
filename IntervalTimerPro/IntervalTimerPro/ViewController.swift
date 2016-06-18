6bd45637b10addb8ca16510894e38fb0f8acb1bc
masa <mn6031@hotmail.co.jp>
2016-06-18 19:12:45 +0900

    secondview追加

---
+++ IntervalTimerPro/IntervalTimerPro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IntervalTimerPro
+//
+//  Created by masa on 2016/06/18.
+//  Copyright © 2016年 Tryworks-Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var bgImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
